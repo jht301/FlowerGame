d13194ce62b6f48ae56febd74085ede5a4c6150a
Unknown <jackhterzi@gmail.com>
2018-03-19 14:11:50 -0400

    ui and rotation done working on function

---
+++ Assets/CameraRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour {
+    public GameObject flower;
+    public float rotateSpeed;
+	// Use this for initialization
+	void Start () {
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(flower.transform);
+       
+        if (Input.GetKey(KeyCode.A)&&transform.position.x>-7) {
+            transform.Translate(Vector3.left*Time.deltaTime*rotateSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) && transform.position.x < 7) {
+            transform.Translate(Vector3.right*Time.deltaTime*rotateSpeed);
+        }
+    }
+}
---
+++ Assets/Manager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour {
+    public static Manager me;
+    public GameObject flower, scissors, water, shovel;
+
+    public int stage;
+
+    public TextMesh guideText;
+
+    public bool scissorsInHand, waterInHand, shovelInHand, clickedOnPlant;
+	// Use this for initialization
+	void Awake () {
+		if(me == null) {
+            me = this;
+        }
+        else {
+            Destroy(this);
+        }
+	}
+
+
+
+	void Start() {
+        stage = 1;
+        guideText.text = "Plant a hole for the seed";
+
+    }
+
+
+
+
+	void Update () {
+        if(stage == 1) {
+            if(clickedOnPlant && shovelInHand) {
+                stage += 1;
+                clickedOnPlant = false;
+            }
+            if(clickedOnPlant && waterInHand) {
+                guideText.text = "There's nothing to water, Try the Shovel";
+                clickedOnPlant = false;
+            }
+            if(clickedOnPlant && scissorsInHand) {
+                guideText.text = "There's nothing to cut, Try the Shovel";
+                clickedOnPlant = false;
+            }
+        }
+		if(stage == 2) {
+            
+            if (clickedOnPlant && shovelInHand) {
+                stage += 1;
+                clickedOnPlant = false;
+            }
+            if (clickedOnPlant && waterInHand) {
+                guideText.text = "There's nothing to water, Try the Shovel";
+                clickedOnPlant = false;
+            }
+            if (clickedOnPlant && scissorsInHand) {
+                guideText.text = "There's nothing to cut, Try the Shovel";
+                clickedOnPlant = false;
+            }
+        }
+        if(stage == 3) {
+
+        }
+        if(stage == 4) {
+
+        }
+	}
+
+    void Holding(string beingHeld) {
+        Debug.Log("I'm holding a " + beingHeld);
+        if(beingHeld == "Scissors") {
+            scissorsInHand = true;
+            waterInHand = false;
+            shovelInHand = false;
+        }else
+        if(beingHeld == "Water") {
+            waterInHand = true;
+            shovelInHand = false;
+            scissorsInHand = false;
+        }else
+        if(beingHeld == "Shovel") {
+            shovelInHand = true;
+            waterInHand = false;
+            scissorsInHand = false;
+        }
+    }
+}
